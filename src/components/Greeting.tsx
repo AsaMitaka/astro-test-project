a73fe49f316e646cec43121c17b4d1a16cfd4b98
AsaMitaka <cepelits@gmail.com>
2023-09-03 22:47:44 +0300

    astro components

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,17 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function Greeting({ messages }) {
+  const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you from u visit</h3>
+      <button class="greeting--btn" onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  );
+}
